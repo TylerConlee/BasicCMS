ccb3375b71592e00f04fc3abe018aefdc662a00e
Tyler Conlee <tylerlconlee@gmail.com>
2016-11-04 08:26:19 -0700

    adjusted basictest and added trash test

--- tests/BasicTest.php
+++ tests/BasicTest.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class BasicTest extends TestCase
 {
     /**
      * A basic functional test example.
@@ -21,5 +21,10 @@ class ExampleTest extends TestCase
     {
         $this->visit('/page/create')
             ->see('Title');
+    }
+    public function testTrashPage()
+    {
+        $this->visit('/admin/trash')
+            ->see('Actions')
     }
 }
